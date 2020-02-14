0f4ac7045bdc7db3b3d1cf61d38c290542827b6c
Ramesh Dahal <rameshdahal2013@gmail.com>
2020-02-14 16:18:48 -0600

    score display added

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-const correctAnswers = ['B', 'B', 'B', 'B'];
+const correctAnswers = ['B', 'B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [
+        form.q1.value,
+        form.q2.value,
+        form.q3.value,
+        form.q4.value,
+        form.q5.value,
+    ];
+
+    //Check answers
+    userAnswers.forEach((answers, index) => {
+        if(answers === correctAnswers[index]){
+          score += 20;   
+        }
+    });
+
+    // Show result on page 
+    result.querySelector('span').textContent = `${score}%`;
+    result.style.display = 'block';
+    // console.log(score);
+    scrollTo(0,0);
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        }else{
+            output++;
+        }
+    }, 20);
+});
+
     
-})
